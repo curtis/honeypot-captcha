387447310df2302241500bd85e544da9c1d706f7
Dave Tapley <dukedave@gmail.com>
2015-02-10 15:16:07 -0700

    Dummy Rails apps to test 3.2.21 and 4.2.0 compatibility

---
+++ dummy/rails-3.2.21/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3221::Application.config.secret_token = '841818ddea853b68f15c786923fae83edc9811fae0074f3f489931a8b782fb96a317eb1dced1ffc656585c89f116328251eafe30cb4c18e7e7e89d1b34d44f8a'
