c76d06718b4bf3dc9638aae57baf9e745022b750
Rob Chekaluk <rchekaluk@ppolitics.org>
2019-02-08 06:51:25 -0500

    Get tests to execute

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ source "http://rubygems.org"
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
 group :development do
+  gem "actionview"
+  gem "activesupport"
   gem "rspec",     "~> 3.8"
   gem "rdoc",      "~> 6.1"
   gem "bundler",   "~> 2"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,14 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
+require 'active_support/concern'
+require 'action_view/helpers/capture_helper'
+require 'action_view/helpers/output_safety_helper'
+require 'action_view/helpers/sanitize_helper'
+require 'action_view/helpers/tag_helper'
+require 'action_view/helpers/text_helper'
+require 'action_view/helpers/url_helper'
+require 'action_view/helpers/form_tag_helper'
 require 'honeypot-captcha'
 
 # Requires supporting files with custom matchers and macros, etc,
